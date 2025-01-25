4f961455c9c5c6e3c6e5cb7ebafaee09b58d0588
inhouuu <fabiobrasil2002@hotmail.com>
2025-01-25 19:31:04 -0300

    Chat Game Teste Socket

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+//instancias
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'app/index.html'));
+});
+
+
+io.on('connection', (socket) => {
+    // console.log('a user connected');
+    // socket.on('disconnect', () => {
+    //     console.log('user disconnected');
+    // });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor ON'); 
+});
